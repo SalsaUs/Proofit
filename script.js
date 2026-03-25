d85094af354f1a3dffee8b29a5144792ac6148e1
SalsaUs <mrfbelcher@gmail.com>
2026-03-25 12:02:22 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function selectRole(role) {
+  if (role === 'tenant') {
+    alert("Tenant flow coming soon!");
+    // window.location.href = "tenant.html";
+  } else {
+    alert("Agent flow coming soon!");
+    // window.location.href = "agent.html";
+  }
+}
