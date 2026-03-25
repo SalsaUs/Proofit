5cc865abcf5f57dcb3bf6788e5579b0298a88f67
SalsaUs <mrfbelcher@gmail.com>
2026-03-25 13:37:45 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function selectRole(role) {
   if (role === 'tenant') {
-    alert("Tenant flow coming soon!");
-    // window.location.href = "tenant.html";
+    window.location.href = "tenant.html";
   } else {
-    alert("Agent flow coming soon!");
-    // window.location.href = "agent.html";
+    window.location.href = "agent.html";
   }
 }
+
 function goHome() {
   window.location.href = "index.html";
 }
